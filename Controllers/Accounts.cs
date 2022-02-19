280f2307c570c6c5867078aebd212a6441db7fc4
Ivan MSI <arigatory@gmail.com>
2022-02-20 02:16:52 +0300

    Sign up with AWS Cognito and ASP.Net MVC Core

---
+++ Controllers/Accounts.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Accounts;
+using Amazon.AspNetCore.Identity.Cognito;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public Accounts(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password).ConfigureAwait(false);
+
+                if (createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+            }
+            return View(model);
+        }
+    }
+}
